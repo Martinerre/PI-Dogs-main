c45770b2dcc838ba6cbedb1a64aa5be8e0f9a2fb
Martierre <martingracedo@gmail.com>
2024-02-23 22:56:39 -0300

    Carga DB

--- PI-Dogs-main/api/index.js
+++ PI-Dogs-main/api/index.js
@@ -1,29 +1,35 @@
 const server = require('./src/app.js');
+const axios = require('axios');
 const { conn } = require('./src/db.js');
+const {Dog} = require('../api/src/db.js')
 
 // require("./src/db.js") // Para prueba datos DB
 
-const URL = 'https://api.thedogapi.com/v1/breeds' //url img https://api.thedogapi.com/v1/images/search?limit=15&api_key=${API_KEY}
+//const URL = 'https://api.thedogapi.com/v1/breeds' 
+const URL = 'https://api.thedogapi.com/v1/images/search?limit=15&api_key=live_TnHJR3xTl8Xm9eFPiVyrbxLdguvnd8ugbO8js75d8CgE0zxKTQIxrtP2W1UigiZP'
+// ej: ,
+// {
+//   "weight":{"imperial":"6 - 13","metric":"3 - 6"},"height":{"imperial":"9 - 11.5","metric":"23 - 29"},
+//   "id":1,"name":"Affenpinscher","bred_for":"Small rodent hunting, lapdog","breed_group":"Toy","life_span":"10 - 12 years","temperament":"Stubborn, Curious, Playful, Adventurous, Active, Fun-loving","origin":"Germany, France","reference_image_id":"BJa4kxc4X"}
+
 const port = 3001;
 
 const apiToDbb = async () => {
   console.log('api to db');
   try {
     let response = await axios.get(URL);
-    let dataResponse = response.data;
-    dataResponse.map(async (el) => {
-      await Country.create({
-        id: el.alpha3Code,
+    response.map(async (el) => {
+      await Dog.create({
+        id: el.breeds.id,
         name: el.name,
-        flag: el.flag,
-        continent: el.region,
-        subregion: el.subregion,
-        capital: el.capital,
-        area: el.area,
-        population: el.population,
+        image: el.url,
+        altura: el.breeds.height.metric,
+        peso: el.breeds.weight.metric,
+        anios: el.breeds.life_span,
       })
     })
-  } catch (error) {
+  } 
+  catch (error) {
     error.message
   }
 }
@@ -34,3 +40,5 @@ conn.sync({ force: false }).then(() => {
     console.log(`Server listening at ${port}`); // eslint-disable-line no-console
   });
 });
+
+apiToDbb(URL)
--- PI-Dogs-main/api/src/models/Dogs.js
+++ PI-Dogs-main/api/src/models/Dogs.js
@@ -19,46 +19,47 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     altura: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate:
-      {
-        min: 0,
-        max: 5,
-        isEven(value) {
-          if (value < 0 || value > 2) {
-            throw new Error("Altura errónea")
-          }
-        }
-      }
+      type: DataTypes.STRING,
+      // allowNull: false,
+      // validate:
+      // {
+      //   min: 0,
+      //   max: 5,
+      //   isEven(value) {
+      //     if (value < 0 || value > 2) {
+      //       throw new Error("Altura errónea")
+      //     }
+      //   }
+      // }
     },
     peso: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate:
-      {
-        min: 0,
-        max: 130,
-        isEven(value) {
-          if (value < 0 || value > 130) {
-            throw new Error("Peso erróneo")
-          }
-        }
-      }
+      type: DataTypes.STRING,
+      // allowNull: false,
+      // validate:
+      // {
+      //   min: 0,
+      //   max: 130,
+      //   isEven(value) {
+      //     if (value < 0 || value > 130) {
+      //       throw new Error("Peso erróneo")
+      //     }
+      //   }
+      // }
     },
     anios: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate:
-      {
-        min: 0,
-        max: 40,
-        isEven(value) {
-          if (value < 0 || value > 40) {
-            throw new Error("Años no correcto")
-          }
-        }
-      }
+      type: DataTypes.STRING,
+      // allowNull: false,
+      // validate:
+      // {
+      //   min: 0,
+      //   max: 40,
+      //   isEven(value) {
+      //     if (value < 0 || value > 40) {
+      //       throw new Error("Años no correcto")
+      //     }
+      //   }
+      // }
     }
-  }, { timestamps: false });
+  }, { timestamps: true }
+  );
 };
